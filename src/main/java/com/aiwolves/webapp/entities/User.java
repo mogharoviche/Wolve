02fc9fe74ad10ff5d314370d9da7b72f1f0d219f
mogharoviche <mogharoviche@gmail.com>
2024-01-02 00:25:18 +0100

    Initial project
    Initial commit (Not yet analysed or tested).

--- src/main/java/com/aiwolves/webapp/entities/User.java
+++ src/main/java/com/aiwolves/webapp/entities/User.java
@@ -76,7 +76,7 @@ public class User implements UserDetails {
 	    @Override
 	    public boolean isEnabled() {
 	        return true;
-	    }
+	    } 
 
 		public User setUsername(String username) {
 			this.username = username;
